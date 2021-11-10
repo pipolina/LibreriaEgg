fcc61cdb00bd33885f98e7282362183c70279c83
Carolina Puebla <puebla.carolina@gmail.com>
2021-11-09 21:17:51 -0300

    Libro Controlador en proceso

--- src/main/java/com/libreria/demo/controladores/LibroController.java
+++ src/main/java/com/libreria/demo/controladores/LibroController.java
@@ -1,8 +1,42 @@
 package com.libreria.demo.controladores;
 
+import com.libreria.demo.excepciones.MiExcepcion;
+import com.libreria.demo.service.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/libro")
 public class LibroController {
+    
+    @Autowired
+    private LibroService libroService;
+    
+    @GetMapping("/guardar")
+    public String formularioLibro(){
+        return "form-libro";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardarLibro(ModelMap modelo,@RequestParam Long isbn, @RequestParam String titulo, @RequestParam Integer anio, @RequestParam Integer ejemplares, @RequestParam String autor, @RequestParam String editorial) throws MiExcepcion{
+        
+        try{
+            libroService.guardarLibro(isbn, titulo, anio, ejemplares, autor, editorial);
+            
+            modelo.put("exito", "Libro guardado con Éxito");
+            return "form-libro";
+            
+        }catch(Exception e){
+            
+            modelo.put("error", "Hubo un error al guardar el libro");
+            return "form-libro";
+        }
+        
+    }
 
 }
